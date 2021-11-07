756e4314849c5dec7c42ef758c1d08d494e09563
Larassa <Larassa@lusi.com>
2021-11-07 21:09:35 +0800

    react-app-ts:hoc1

---
+++ api-example/typescript/interfaces/01.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description: enter your description
+ * @Date: 2021-10-30 22:49:43
+ * @LastEditTime: 2021-10-30 22:58:05
+ */
+class temp {
+  public a(data: string) {}
+  public a(age: string, data: { name: string }) {}
+}
+
+const t = new temp();
+t.a('aaa', { name: 't' });
