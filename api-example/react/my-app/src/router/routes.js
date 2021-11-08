fcbde83bc92a547e09627dea26fa21c673095203
DESKTOP-AABLQ21\shifang <si.lu@tenclass.com>
2021-11-08 12:08:46 +0800

    取消注释

--- api-example/react/my-app/src/router/routes.js
+++ api-example/react/my-app/src/router/routes.js
@@ -1,14 +1,14 @@
 /*
  * @Description:
  * @Date: 2021-04-26 22:00:23
- * @LastEditTime: 2021-08-19 14:27:27
+ * @LastEditTime: 2021-11-08 12:08:21
  */
 import React from 'react';
 import Home from '../components/Home';
 import About from '../components/About';
-// import RefIndex from '../pages/ref/Index';
-// import RefIndex1 from '../pages/ref/Index1';
-// import RefIndex2 from '../pages/ref/Index2';
+import RefIndex from '../pages/ref/Index';
+import RefIndex1 from '../pages/ref/Index1';
+import RefIndex2 from '../pages/ref/Index2';
 import ReducerIndex from '../pages/reducer/Index';
 import Observable from '../pages/mobx/observable';
 import MobxToJS from '../pages/mobx/toJS';
@@ -33,18 +33,18 @@ export const ROUTES = [
       },
     ],
   },
-//   {
-//     path: '/ref',
-//     component: RefIndex,
-//   },
-//   {
-//     path: '/ref1',
-//     component: RefIndex1,
-//   },
-//   {
-//     path: '/ref2',
-//     component: RefIndex2,
-//   },
+  {
+    path: '/ref',
+    component: RefIndex,
+  },
+  {
+    path: '/ref1',
+    component: RefIndex1,
+  },
+  {
+    path: '/ref2',
+    component: RefIndex2,
+  },
   {
     path: '/reducer',
     component: ReducerIndex,
