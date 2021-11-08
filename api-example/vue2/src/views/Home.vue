58db529bf87f42050911a4026dd5f218e4ba6466
lusi <si.lu@tenclass.com>
2021-11-08 10:27:53 +0800

    import

--- api-example/vue2/src/views/Home.vue
+++ api-example/vue2/src/views/Home.vue
@@ -1,27 +1,27 @@
 <!--
  * @Author: your name
  * @Date: 2021-04-08 11:00:05
- * @LastEditTime: 2021-05-21 16:46:49
- * @LastEditors: larassa
+ * @LastEditTime: 2021-09-30 17:42:50
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \larassa-blog\api-example\vue2\src\views\Home.vue
 -->
 <template>
   <div class="home">
     home
-		<!-- <li v-for="item in list" :key="item.name">{{ item.name }}</li> -->
+    <!-- <li v-for="item in list" :key="item.name">{{ item.name }}</li> -->
     <div>{{ list }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
-	data() {
-		return {
-			list: []
-		}
-	},
+  name: 'Home',
+  data() {
+    return {
+      list: [],
+    };
+  },
   created() {
     // this.$http({
     // 	url: 'api/v2/admin/refundlist',
@@ -36,9 +36,9 @@ export default {
     //     order_type: 'desc',
     //   }
     // }).then(res => {
-		// 	console.log('res', res)
-		// 	this.list = res
+    // 	console.log('res', res)
+    // 	this.list = res
     // })
-  }
+  },
 };
 </script>
